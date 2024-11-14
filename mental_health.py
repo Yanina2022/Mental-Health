0ff7f8623a49ad42a20c81890619574b8bbc1fdb
Yanina Soto <102045041+Yanina2022@users.noreply.github.com>
2024-11-13 21:14:00 -0300

    Add files via upload

---
+++ mental_health.py
@@ -0,0 +1,12 @@
+
+
+import pandas as pd
+import numpy as pd
+
+import pandas as pd
+
+# Reemplaza 'ruta/del/archivo.csv' con la ruta de tu archivo .csv
+df = pd.read_csv('c:/Users/yana/Desktop/mental_health/mental_health_dataset.csv.csv')
+
+# Opcional: Muestra las primeras filas del archivo
+print(df.head())
